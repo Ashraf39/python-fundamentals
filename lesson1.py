0248ec29f399c0754a73e53dc3280301d1a4d658
Ashraf Bhuiyan <99349346+Ashraf39@users.noreply.github.com>
2026-01-19 12:56:36 +0600

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,5 @@
 #First Program : Ask user for name and birth year. Then print name and age of the user
-from datetime import datetime
+from datetime import datetime #for getting datetime dynamically
 name = input("Input your name : ")
 birth_year = int(input("Input your birth year : "))
 age = datetime.now().year - birth_year
